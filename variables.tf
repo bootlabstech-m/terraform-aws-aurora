3c0d00742fe19da858ba2aca43b85860e1b114a1
Silpashree <silpashree@bootlabstech.com>
2023-01-22 13:47:15 +0530

    initial

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aurora_details" {
+  description = "The aurora cluster details"
+  type        = list(any)
+}
+variable "region" {
+  description = "The region for the resource"
+  type        = string
+}
+variable "role_arn" {
+  description = " The ARN of the IAM role"
+  type = string
+}
